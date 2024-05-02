75b0f35d02444abae7ccdfd1a547a4a4fb68fb07
Andreas Askholm <andreas_askholm@hotmail.com>
2024-05-02 20:33:29 +0200

    files now work as expected at least in dev mode

--- GUI/UI/UI/settings.py
+++ GUI/UI/UI/settings.py
@@ -117,7 +117,7 @@ AUTH_USER_MODEL = "frontend.User"
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'da-dk'
 
 TIME_ZONE = 'Europe/Berlin'
 
@@ -131,6 +131,9 @@ USE_TZ = True
 STATIC_ROOT = BASE_DIR / 'static/'
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [ BASE_DIR / "frontend/static",]
+
+MEDIA_ROOT = BASE_DIR / 'media/'
+MEDIA_URL = 'media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
--- GUI/UI/UI/urls.py
+++ GUI/UI/UI/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include,re_path
 from django.views.static import serve
+from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
@@ -24,4 +25,4 @@ urlpatterns = [
     path('admin-bi/', admin.site.urls),
     # TODO probably remove this when we get an actual 'server' of sites
     re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
