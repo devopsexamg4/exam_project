5a4b20aa5a7dd65987c18acc9b8ead2e515035d3
Andreas Askholm <andreas_askholm@hotmail.com>
2024-05-15 17:04:27 +0200

    should now be configured to use postgres as db, remember to set the secrets

--- GUI/UI/UI/settings.py
+++ GUI/UI/UI/settings.py
@@ -83,13 +83,25 @@ WSGI_APPLICATION = 'UI.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+ENGINE = config('DB_ENGINE')
+if config('DB_ENGINE').split('.')[-1] == 'sqlite3':
+    DATABASES = {
+        'default': {
+            'ENGINE':ENGINE ,
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE':ENGINE ,
+            'NAME': 'postgresdb',
+            'USER':config('DB_USERNAME'),
+            'PASSWORD':config('DB_PASSWORD'),
+            'HOST':config('DB_HOST'),
+            'PORT':config('DB_PORT'),
+        }
+    }
 
 
 # Password validation
