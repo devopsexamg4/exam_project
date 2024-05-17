2d583a474f5dcc166295af4a8c56b6f126d1788d
Kaaasper <kasperhoflund@gmail.com>
2024-05-17 18:32:07 +0200

    Finally fixed submission assignment key test issue

--- GUI/UI/tests/test_views.py
+++ GUI/UI/tests/test_views.py
@@ -46,19 +46,22 @@ class AdminViewTest(test.MessagesTestMixin, TestCase):
         response2 = self.client.post(reverse('admin'), {'pk': self.user_student.pk, 'type': "invalid"})
         self.assertMessages(response2, [Message(level=40, message={'type': ['Select a valid choice. invalid is not one of the available choices.']})])
 
-class StudentViewTest(TestCase):
+class StudentViewTest(test.MessagesTestMixin, TestCase):
     def setUp(self):
         self.client = Client()
+        self.assignment = Assignments.objects.create(
+            title="Test Assignment",
+            dockerfile=SimpleUploadedFile("file.txt", b"file_content"),
+            status=Assignments.StatusChoices.ACTIVE
+        )
         self.user_student = User.objects.create(username='user_student', type=User.TypeChoices.STUDENT)
         self.user_teacher = User.objects.create(username='user_teacher', type=User.TypeChoices.TEACHER)
         self.user_student.set_password('12345')
         self.user_teacher.set_password('12345')
+        self.user_student.assignments.add(self.assignment)
         self.user_student.save()
         self.user_teacher.save()
-        self.assignment = Assignments.objects.create(
-            title="Test Assignment",
-            dockerfile=SimpleUploadedFile("file.txt", b"file_content"),
-        )
+
 
 
     def test_student_access(self):
@@ -74,11 +77,10 @@ class StudentViewTest(TestCase):
     def test_valid_form_submission(self):
         self.client.login(username='user_student', password='12345')
         response = self.client.post(reverse('student'), {'student-pk' : self.user_student.pk, 
-                                                         'assignment' : self.assignment, 
+                                                         'assignment' : self.assignment.pk, 
                                                          'File' : SimpleUploadedFile("file.txt", b"file_content")})
         self.assertEqual(response.status_code, 200)
-        
-        print(list(get_messages(response.wsgi_request)))
+        self.assertMessages(response, [Message(level=20, message='submission received')])
 
 
 class ViewStudentViewTest(TestCase):
