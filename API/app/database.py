e4899f0e27d9437d867e40d70c7fc96a7ac3e0bb
Caerullean <cerulean@DESKTOP-E4M2BVS>
2024-05-19 15:38:11 +0200

    moved env variables to .env

--- API/app/database.py
+++ API/app/database.py
@@ -2,13 +2,15 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
 import os
 
-host = os.getenv("DB_HOST", "pservice")
-port = os.getenv("DB_PORT", "5432")
-dbname = os.getenv("DB_NAME", "test")
-user = os.getenv("DB_USER", "postgres")
-password = os.getenv("DB_PASSWORD", "12345")
+load_dotenv()
+host = os.getenv("DB_HOST")
+port = os.getenv("DB_PORT")
+dbname = os.getenv("DB_NAME")
+user = os.getenv("DB_USER")
+password = os.getenv("DB_PASSWORD")
 
 SQLALCHEMY_DATABASE_URL = f"postgresql://{user}:{password}@{host}:{port}/{dbname}" # expects these values as environt variables
 
