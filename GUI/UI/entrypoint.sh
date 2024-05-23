bb5026aba6e7d547a0f8d62b64314d8324973825
Kristoffer Klokker <kristoffer.klokker@gmail.com>
2024-05-23 12:49:16 -0700

    Fixed problem with google secrets introducting hidden characters

--- GUI/UI/entrypoint.sh
+++ GUI/UI/entrypoint.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 export $(grep -v '^#' .env | xargs)
-
+# Iterate over all environment variables and strip '\r' and '\n' characters
+for var in $(compgen -e); do
+    export $var=$(echo -n "${!var}" | tr -d '\r\n')
+done
 
 service cron start
 
