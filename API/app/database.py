62bf1606f6bdbbc66470e33832823dc53dd0b107
Caerullean <cerulean@DESKTOP-E4M2BVS>
2024-05-24 11:29:46 +0200

    changed database to production database

--- API/app/database.py
+++ API/app/database.py
@@ -13,10 +13,10 @@ user = os.getenv("DB_USER")
 password = os.getenv("DB_PASSWORD")
 
 
-# SQLALCHEMY_DATABASE_URL = f"postgresql://{user}:{password}@{host}:{port}/{dbname}" # expects these values as environt variables
-# engine = create_engine(SQLALCHEMY_DATABASE_URL)
+SQLALCHEMY_DATABASE_URL = f"postgresql://{user}:{password}@{host}:{port}/{dbname}" # expects these values as environt variables
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+#SQLALCHEMY_DATABASE_URL_TEST = "sqlite:///./sql_app.db"
+#engine_test = create_engine(SQLALCHEMY_DATABASE_URL_TEST, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
